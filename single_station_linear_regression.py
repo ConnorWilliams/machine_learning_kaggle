643053b72b81340390130774d7647f56360f9c4d
Stephen Thomson <steve@linux-29o7.site>
2015-12-03 15:06:01 +0000

    generate sub.csv

--- single_station_linear_regression.py
+++ single_station_linear_regression.py
@@ -30,7 +30,7 @@ filestring = 'Train/station_201_deploy.csv'
 # Read in training and test data
 training_features = training_data = np.genfromtxt(filestring, dtype=float, comments='#', delimiter=',',
                   skip_header=1, skip_footer=0, converters=None, missing_values={"NA"},
-                  filling_values='0', usecols=features_num,
+                  filling_values='0', usecols=(1,2),
                   names=None, excludelist=None, deletechars=None, replace_space='_',
                   autostrip=False, case_sensitive=True, defaultfmt='f%i',
                   unpack=None, usemask=False, loose=True, invalid_raise=True)
@@ -56,8 +56,13 @@ test_features = np.genfromtxt('test.csv', dtype=float, comments='#', delimiter='
 clf = linear_model.LinearRegression()
 clf.fit (training_features, training_target)
 print('Coefficients: \n', clf.coef_)
+output = open("sub.csv","w")
+preds = clf.predict(test_features)
+output.write("Id,\"bikes\"")
+for x in range(0,preds.size):
+      output.write(str(x)+","+str(preds[x])+ "\n")
+output.close()
 
-print clf.predict(test_features)
 
 # print("Residual sum of squares: %.2f" % np.mean((clf.predict(test_features) - test_target) ** 2))
 # print('Variance score: %.2f' % clf.score(test_features, test_target))
