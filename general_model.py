f164b140f1bfb58728c25a616f51a937762ac468
Connor <cw13121@my.bristol.ac.uk>
2015-12-10 17:17:02 +0000

    individual model and general model phase 1 working

--- general_model.py
+++ general_model.py
@@ -22,7 +22,7 @@ if (len(sys.argv)==1):
     print 'Making test predictions...'
 elif (sys.argv)[1] != '-m':
     mock = 0
-    test_file = 'mock_test.csv'
+    test_file = 'test.csv'
     print 'Making test predictions...'
 elif (sys.argv)[1] == '-m':
     mock = 1
@@ -43,7 +43,8 @@ test_feat =    ["Id","station","latitude","longitude","numDocks","timestamp",
                 "bikes_3h_ago","full_profile_3h_diff_bikes","full_profile_bikes",
                 "short_profile_3h_diff_bikes","short_profile_bikes"]
 
-selectedFeatures = ["day","weekhour","isHoliday","windMaxSpeed.m.s",
+selectedFeatures = ["station","latitude","longitude","numDocks","day",
+                "weekhour","isHoliday","windMaxSpeed.m.s",
                 "windMeanSpeed.m.s","windDirection.grades","temperature.C",
                 "relHumidity.HR","airPressure.mb","precipitation.l.m2",
                 "bikes_3h_ago",
@@ -58,7 +59,7 @@ for x in selectedFeatures:
     training_feature_cols = training_feature_cols + (trainIdx,)
     test_feature_cols = test_feature_cols + (testIdx,)
 
-all_test_features = np.genfromtxt(test_file, dtype=float, comments='#', delimiter=',',
+test_features = np.genfromtxt(test_file, dtype=float, comments='#', delimiter=',',
                   skip_header=1, skip_footer=0, converters=None, missing_values={"NA"},
                   filling_values='0', usecols=test_feature_cols,
                   names=None, excludelist=None, deletechars=None, replace_space='_',
@@ -79,69 +80,64 @@ if mock:
             autostrip=False, case_sensitive=True, defaultfmt='f%i',
             unpack=None, usemask=False, loose=True, invalid_raise=True)
 
-output = open("individual_sub.csv","w")
+output = open("general_sub.csv","w")
 output.write("Id,\"bikes\"" +"\n")
 predictions = []
 
-# For all test data files
-for x in range(201,276):
-    if mock:
-        filestring = 'Train/mock_station_' +str(x)+'_deploy.csv'
-    else:
-        filestring = 'Train/station_' +str(x)+'_deploy.csv'
+if mock:
+    training_file = 'mock_general_station_train.csv'
+else:
+    training_file = 'general_station_train.csv'
 
 # Read in the training features and target variable
-    training_features = np.genfromtxt(filestring, dtype=float, comments='#', delimiter=',',
-        skip_header=1, skip_footer=0, converters=None, missing_values={"NA"},
-        filling_values=0, usecols=training_feature_cols,
-        names=None, excludelist=None, deletechars=None, replace_space='_',
-        autostrip=False, case_sensitive=True, defaultfmt='f%i',
-        unpack=None, usemask=False, loose=True, invalid_raise=True)
-    training_target = np.genfromtxt(filestring, dtype=float, comments='#', delimiter=',',
-        skip_header=1, skip_footer=0, converters=None, missing_values={"NA"},
-        filling_values=0, usecols=train_feat.index("bikes"),
-        names=None, excludelist=None, deletechars=None, replace_space='_',
-        autostrip=False, case_sensitive=True, defaultfmt='f%i',
-        unpack=None, usemask=False, loose=True, invalid_raise=True)
-
-# Get the right test features for this station
-    station_test_features = []
-    first_test = (x-201)*30
-    for i in range(first_test,first_test+30):
-            station_test_features.append(all_test_features[i].tolist())
+training_features = np.genfromtxt(training_file, dtype=float, comments='#', delimiter=',',
+    skip_header=1, skip_footer=0, converters=None, missing_values={"NA"},
+    filling_values=0, usecols=training_feature_cols,
+    names=None, excludelist=None, deletechars=None, replace_space='_',
+    autostrip=False, case_sensitive=True, defaultfmt='f%i',
+    unpack=None, usemask=False, loose=True, invalid_raise=True)
+training_target = np.genfromtxt(training_file, dtype=float, comments='#', delimiter=',',
+    skip_header=1, skip_footer=0, converters=None, missing_values={"NA"},
+    filling_values=0, usecols=train_feat.index("bikes"),
+    names=None, excludelist=None, deletechars=None, replace_space='_',
+    autostrip=False, case_sensitive=True, defaultfmt='f%i',
+    unpack=None, usemask=False, loose=True, invalid_raise=True)
 
 # Select our features
 # Either by removing ones with low variance:
-    # selector = VarianceThreshold()
-    # training_features = selector.fit_transform(training_features, training_target)
-    # station_test_features = selector.transform(station_test_features)
+# selector = VarianceThreshold()
+# training_features = selector.fit_transform(training_features, training_target)
+# station_test_features = selector.transform(station_test_features)
 
 # Or by K best:
-    selector = SelectKBest(f_regression, k=4)
-    training_features = selector.fit_transform(training_features, training_target)
-    station_test_features = selector.transform(station_test_features)
+selector = SelectKBest(f_regression, k=4)
+training_features = selector.fit_transform(training_features, training_target)
+test_features = selector.transform(test_features)
+
+print '\ntest_features.shape = ', test_features.shape
+print 'training_features.shape = ',training_features.shape
+print 'training_target.shape = ',training_target.shape
 
 # Print the features we have chosen for this station
-    # print '\nFeatures for station ' + str(x) + ':'
-    # for idx in range(0, len(selector.get_support())):
-    #     if selector.get_support()[idx] == True:
-    #         print '\t' + str(selectedFeatures[idx])
-    # print '\ntraining_features 0, 10, 235:\n', training_features[0], '\n', training_features[10], '\n', training_features[235]
-    # print '\nstation_test_features 0, 10, 20:\n', station_test_features[0], '\n', station_test_features[10], '\n', station_test_features[25]
-    # raw_input("Press enter...")
+print '\nFeatures selcted:'
+for idx in range(0, len(selector.get_support())):
+    if selector.get_support()[idx] == True:
+        print '\t' + str(selectedFeatures[idx])
+print '\ntraining_features 0, 10, 235:\n', training_features[0], '\n', training_features[10], '\n', training_features[235]
+print '\ntest_features 0, 10, 20:\n', test_features[0], '\n', test_features[10], '\n', test_features[25]
 
 # Generate a model for this particular station
-    clf = linear_model.LinearRegression(fit_intercept=True, normalize=False, copy_X=True, n_jobs=1)
-    # clf = linear_model.Lars(fit_intercept=True, verbose=False, normalize=True, precompute='auto', n_nonzero_coefs=500, eps=2.2204460492503131e-16, copy_X=True, fit_path=True, positive=False)
-    # clf = linear_model.Ridge(alpha=1.0, fit_intercept=True, normalize=False, copy_X=True, max_iter=None, tol=0.001, solver='auto', random_state=None)
-    # clf = linear_model.BayesianRidge(n_iter=300, tol=0.001, alpha_1=1e-06, alpha_2=1e-06, lambda_1=1e-06, lambda_2=1e-06, compute_score=False, fit_intercept=True, normalize=False, copy_X=True, verbose=False)
-    # clf = linear_model.Perceptron(penalty=None, alpha=0.0001, fit_intercept=True, n_iter=5, shuffle=True, verbose=0, eta0=1.0, n_jobs=1, random_state=0, class_weight=None, warm_start=False)
-    # clf = svm.SVR(kernel='rbf', degree=3, gamma='auto', coef0=0.0, tol=0.001, C=1.0, epsilon=0.1, shrinking=True, cache_size=200, verbose=False, max_iter=-1)
-    # clf = tree.DecisionTreeRegressor(criterion='mse', splitter='best', max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, random_state=None, max_leaf_nodes=None, presort=False)
-    clf.fit (training_features, training_target)
-    #print 'Coefficients: \n\t', clf.coef_
+clf = linear_model.LinearRegression(fit_intercept=True, normalize=False, copy_X=True, n_jobs=1)
+# clf = linear_model.Lars(fit_intercept=True, verbose=False, normalize=True, precompute='auto', n_nonzero_coefs=500, eps=2.2204460492503131e-16, copy_X=True, fit_path=True, positive=False)
+# clf = linear_model.Ridge(alpha=1.0, fit_intercept=True, normalize=False, copy_X=True, max_iter=None, tol=0.001, solver='auto', random_state=None)
+# clf = linear_model.BayesianRidge(n_iter=300, tol=0.001, alpha_1=1e-06, alpha_2=1e-06, lambda_1=1e-06, lambda_2=1e-06, compute_score=False, fit_intercept=True, normalize=False, copy_X=True, verbose=False)
+# clf = linear_model.Perceptron(penalty=None, alpha=0.0001, fit_intercept=True, n_iter=5, shuffle=True, verbose=0, eta0=1.0, n_jobs=1, random_state=0, class_weight=None, warm_start=False)
+# clf = svm.SVR(kernel='rbf', degree=3, gamma='auto', coef0=0.0, tol=0.001, C=1.0, epsilon=0.1, shrinking=True, cache_size=200, verbose=False, max_iter=-1)
+# clf = tree.DecisionTreeRegressor(criterion='mse', splitter='best', max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, random_state=None, max_leaf_nodes=None, presort=False)
+clf.fit (training_features, training_target)
+# print 'Coefficients: \n\t', clf.coef_
 
-    predictions.extend(clf.predict(station_test_features).tolist())
+predictions.extend(clf.predict(test_features).tolist())
 
 for p in range(0, len(predictions)):
     output.write(str(p+1) + "," + str(predictions[p]) +"\n")
